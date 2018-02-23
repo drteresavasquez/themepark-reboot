c17dea707fb8dedd4ee6ae7d37fe08262f506d0c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-02-22 20:32:02 -0600

    updated layout

--- javascripts/dom_elements.js
+++ javascripts/dom_elements.js
@@ -3,7 +3,7 @@
 console.log("the DOM");
 let dataCalls = require("./data-calls.js");
 
-let images = ["https://i1.wp.com/howtobeagraduate.com/wp-content/uploads/2017/06/Disneyland_Paris_Park_mainstreetsquare.jpg", "https://cdn.cnn.com/cnnnext/dam/assets/141016172023-future-theme-parks-wanda-hefei-opera-garden-horizontal-large-gallery.jpg", "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-assets/parks-and-tickets/attractions/magic-kingdom/walt-disney-world-railroad-frontierland/walt-disney-world-railroad-frontierland-00.jpg?21072014093146", "https://www.disneyfanatic.com/wp-content/uploads/2017/04/Liberty-Bell-replica-and-Hall-of-Presidents-liberty-square-magic-kingdom-walt-disney-world-2.jpg", "http://www.bestoforlando.com/articles/Walt-Disney-World-Fantasyland-to-Get-Makeover/images/BOO_Large_New-Fantasyland_at-Magic-Kingdom-at-Walt-Disney-World-Blog.jpg", "https://i0.wp.com/media2.slashfilm.com/slashfilm/wp/wp-content/images/tomorrowland-440x298.jpg", "https://vignette.wikia.nocookie.net/disneyparksfanon/images/2/21/Cinderella_Castle.png/revision/latest/scale-to-width-down/640?cb=20160204211208", "https://hello.travefy.com/wp-content/uploads/2014/12/free-itinerary-builder-maker-travefy.png"];
+let images = ["https://i1.wp.com/howtobeagraduate.com/wp-content/uploads/2017/06/Disneyland_Paris_Park_mainstreetsquare.jpg", "https://cdn.cnn.com/cnnnext/dam/assets/141016172023-future-theme-parks-wanda-hefei-opera-garden-horizontal-large-gallery.jpg", "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-assets/parks-and-tickets/attractions/magic-kingdom/walt-disney-world-railroad-frontierland/walt-disney-world-railroad-frontierland-00.jpg?21072014093146", "https://www.disneyfanatic.com/wp-content/uploads/2017/04/Liberty-Bell-replica-and-Hall-of-Presidents-liberty-square-magic-kingdom-walt-disney-world-2.jpg", "http://www.bestoforlando.com/articles/Walt-Disney-World-Fantasyland-to-Get-Makeover/images/BOO_Large_New-Fantasyland_at-Magic-Kingdom-at-Walt-Disney-World-Blog.jpg", "https://vignette.wikia.nocookie.net/disney/images/d/d3/Tomorrowland_Magic_Kingdom.jpg/revision/latest/scale-to-width-down/350?cb=20120107183212", "https://vignette.wikia.nocookie.net/disneyparksfanon/images/2/21/Cinderella_Castle.png/revision/latest/scale-to-width-down/640?cb=20160204211208", "https://hello.travefy.com/wp-content/uploads/2014/12/free-itinerary-builder-maker-travefy.png"];
 
 let date = new Date();
 let yyyy = date.getFullYear();
@@ -19,9 +19,11 @@ dataCalls.getAllAreas()
         <div class="card">
     <div class="card-image activator waves-effect waves-block waves-light">
       <img class="activator" src="${images[results[item].id - 1]}">
-      <span class="card-title activator grey-text text-darken-4">${results[item].name}<i class="material-icons right">more_vert</i></span>
+      <span class="card-title activator grey-text text-darken-4">${results[item].name}<i class="material-icons right">more_vert</i></span>    
+      </div>
+      <div class="card-content">
+      <p><a href="#" style="color:#${results[item].colorTheme}">(Area ${results[item].id}) Attractions</a></p>
     </div>
-
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${results[item].name}<i class="material-icons right">close</i></span>
       <p>${results[item].description}</p>
@@ -33,4 +35,5 @@ dataCalls.getAllAreas()
 
         });
         
+
     });
